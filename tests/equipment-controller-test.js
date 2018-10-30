eb1a5ec2d9358cded25ec52d8917d53037a5738a
jkrayer <jameskrayer@yahoo.com>
2018-10-30 08:26:23 -0400

    Add euqipment controller test

---
+++ tests/equipment-controller-test.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const mocha = require('mocha');
+
+const { get } = require('../lib/controllers/equipment');
+
+function callback(...a) {
+  return a;
+}
+
+describe('get', () => {
+  it('should return a 406 and message when the given name does not exist', () => {
+    assert.deepStrictEqual(get({query: {name: 'Zoomer'}}, callback), [{message: 'Invalid query string'}, 406]);
+    assert.deepStrictEqual(get({query: {name: 'Paladin'}}, callback), [{message: 'Invalid query string'}, 406]);
+  });
+});
+
+// TODO: Add a more malicious test
+describe('get', () => {
+  it('should return a 406 and message when bad data is given', () => {
+    assert.deepStrictEqual(get(null, callback), [{message: 'Invalid query string'}, 406]);
+    assert.deepStrictEqual(get({query: {name: () => {}}}, callback), [{message: 'Invalid query string'}, 406]);
+  });
+});
+
+describe('get', () => {
+  it('should return an object when a valid name is passed', () => {
+    const result = get({query: {name: 'Thief'}}, callback);
+    const resultKeys = Object.keys(result[0]);
+    const dataKeys = Object.keys(result[0].data);
+
+    assert.deepStrictEqual(resultKeys, ['message', 'data']);
+    assert.deepStrictEqual(dataKeys, ['startingMoney', 'equipment', 'armor', 'weapons']);
+  });
+});
+
+describe('get', () => {
+  it('should return an object with filtered weapons and armor', () => {
+    const result = get({query: {name: 'Magic-User'}}, callback);
+    const allowedWeaponNames = result[0].data.weapons.map(a => a.name);
+
+    assert.deepStrictEqual(allowedWeaponNames, ['Dagger', 'Silver Dagger']);
+    assert.deepStrictEqual(result[0].data.armor, []);
+  });
+});
