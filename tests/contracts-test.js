421bcb284223aef951bb45de18cb5988563df465
jkrayer <jameskrayer@yahoo.com>
2018-10-30 18:28:27 -0400

    Add contract for numbers

--- tests/contracts-test.js
+++ tests/contracts-test.js
@@ -3,46 +3,58 @@ const mocha = require('mocha');
 
 const contracts = require('../lib/helpers/contracts');
 
-  // isObject
-  describe('isObject', () => {
-    it('should only return true for {} and Contructor return', () => {
-      const { isObject } = contracts;
-      function A() {}
+// isNumber
+describe('isNumber', () => {
+  it('should only return number for integers, floats and NaN', () => {
+    const { isNumber } = contracts;
 
-      assert.strictEqual(isObject({}), true);
-      assert.strictEqual(isObject(new A()), true);
-      assert.strictEqual(isObject([]), false);
-      assert.strictEqual(isObject(null), false);
-    });
+    assert.strictEqual(isNumber(1), true);
+    assert.strictEqual(isNumber(4.5), true);
+    assert.strictEqual(isNumber(NaN), false);
+    assert.strictEqual(isNumber('7'), false);
   });
+});
 
-  // isString
-  describe('isString', () => {
-    it('should only return true for strings', () => {
-      const { isString } = contracts;
-      let abc;
+// isObject
+describe('isObject', () => {
+  it('should only return true for {} and Contructor return', () => {
+    const { isObject } = contracts;
+    function A() {}
 
-      assert.strictEqual(isString('abc'), true);
-      assert.strictEqual(isString('123'), true);
-      assert.strictEqual(isString(abc), false);
-      assert.strictEqual(isString(123), false);
-    });
+    assert.strictEqual(isObject({}), true);
+    assert.strictEqual(isObject(new A()), true);
+    assert.strictEqual(isObject([]), false);
+    assert.strictEqual(isObject(null), false);
   });
+});
 
-  // isUndefined
-  describe('isUndefined', () => {
-    it('should only return true for undefined', () => {
-      const { isUndefined } = contracts;
-      const o = {};
-      let abc;
+// isString
+describe('isString', () => {
+  it('should only return true for strings', () => {
+    const { isString } = contracts;
+    let abc;
 
-      assert.strictEqual(isUndefined(void 0), true);
-      assert.strictEqual(isUndefined(o.a), true);
-      assert.strictEqual(isUndefined(abc), true);
-      assert.strictEqual(isUndefined(0), false);
-      assert.strictEqual(isUndefined(''), false);
-      assert.strictEqual(isUndefined(false), false);
-      assert.strictEqual(isUndefined(null), false);
-      assert.strictEqual(isUndefined(NaN), false);
-    });
+    assert.strictEqual(isString('abc'), true);
+    assert.strictEqual(isString('123'), true);
+    assert.strictEqual(isString(abc), false);
+    assert.strictEqual(isString(123), false);
   });
+});
+
+// isUndefined
+describe('isUndefined', () => {
+  it('should only return true for undefined', () => {
+    const { isUndefined } = contracts;
+    const o = {};
+    let abc;
+
+    assert.strictEqual(isUndefined(void 0), true);
+    assert.strictEqual(isUndefined(o.a), true);
+    assert.strictEqual(isUndefined(abc), true);
+    assert.strictEqual(isUndefined(0), false);
+    assert.strictEqual(isUndefined(''), false);
+    assert.strictEqual(isUndefined(false), false);
+    assert.strictEqual(isUndefined(null), false);
+    assert.strictEqual(isUndefined(NaN), false);
+  });
+});
